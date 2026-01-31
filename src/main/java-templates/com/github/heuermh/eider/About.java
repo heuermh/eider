772563bb64f4b8edb04b02219a7c3f3c7ecac889
Michael L Heuer <heuermh@acm.org>
2026-01-31 10:10:59 -0600

    specify zenodo doi

--- src/main/java-templates/com/github/heuermh/eider/About.java
+++ src/main/java-templates/com/github/heuermh/eider/About.java
@@ -24,7 +24,7 @@ public final class About implements IVersionProvider {
     private static final String ARTIFACT_ID = "${project.artifactId}";
     private static final String BUILD_TIMESTAMP = "${build-helper-maven-plugin.build.timestamp}";
     private static final String COMMIT = "${git.commit.id}";
-    private static final String DOI = "10.5281/zenodo.XXX";
+    private static final String DOI = "10.5281/zenodo.18443266";
     private static final String COPYRIGHT = "Copyright (c) 2025 held jointly by the individual authors.";
     private static final String LICENSE = "Licensed Apache License 2.0.";
     private static final String VERSION = "${project.version}";
